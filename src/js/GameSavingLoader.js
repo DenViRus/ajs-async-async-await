ca653ff3fe500ef9d8ccf1c426c69fe0a8fe9ae1
Denis Yefremenko <denvirus83@mail.ru>
2021-10-21 13:38:42 +0400

    async, async-await: GameSavingLoader corrected, GameSaving added

--- src/js/GameSavingLoader.js
+++ src/js/GameSavingLoader.js
@@ -1,15 +1,9 @@
 import { read } from "./reader.js";
 import { json } from "./parser.js";
+import { GameSaving } from "./GameSaving.js";
 
 export class GameSavingLoader {
   static async load() {
-    class GameSaving {
-      constructor(data) {
-        this.id = data.id;
-        this.created = data.created;
-        this.userInfo = data.userInfo;
-      }
-    }
     const readed = await read();
     const parsed = await json(readed);
     const data = JSON.parse(parsed);
